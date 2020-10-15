089ae16b86f858412b8de7b60118fc0faa8c0d41
Ricardo <rikardo_kz@live.com.pt>
2020-10-15 19:07:49 +0100

    add create-orphanage html

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,15 +1,16 @@
 const options = {
-    dragging: false,
-    touchZoom: false,
-    doubleClickZoom: false,
-    scrollWheelZoom: false,
-    zoomControl: false
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
 };
 
-const map = L.map('mapid', options).setView([38.711, -8.98], 13);
+const map = L.map("mapid", options).setView([38.711, -8.98], 13);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 // icon created
@@ -17,25 +18,23 @@ const icon = L.icon({
   iconUrl: "./public/images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
+  popupAnchor: [170, 2],
 });
 
 // markers
 
-L.marker([38.711, -8.98], { icon }).addTo(map)
-    
+L.marker([38.711, -8.98], { icon }).addTo(map);
 
 /* image gallery */
-function selectImage(e){
-    const button = e.currentTarget;
-    const mainImg = document.querySelector(".orphanage-details img")
-    const buttons = document.querySelectorAll(".images button")
-    buttons.forEach(button => {
-        button.classList.remove("active");
-    })
-    
-    mainImg.src = e.target.src;
+function selectImage(e) {
+  const button = e.currentTarget;
+  const mainImg = document.querySelector(".orphanage-details img");
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
 
+  mainImg.src = e.target.src;
 
-    button.classList.add("active");
+  button.classList.add("active");
 }
