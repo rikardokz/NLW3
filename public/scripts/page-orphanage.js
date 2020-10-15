0d89886578814c7fbd1761f07c529a9b91965bf6
Ricardo <rikardo_kz@live.com.pt>
2020-10-15 18:40:24 +0100

    add styles and logic to orphanage.html

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,41 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false
+};
+
+const map = L.map('mapid', options).setView([38.711, -8.98], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// icon created
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
+
+// markers
+
+L.marker([38.711, -8.98], { icon }).addTo(map)
+    
+
+/* image gallery */
+function selectImage(e){
+    const button = e.currentTarget;
+    const mainImg = document.querySelector(".orphanage-details img")
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(button => {
+        button.classList.remove("active");
+    })
+    
+    mainImg.src = e.target.src;
+
+
+    button.classList.add("active");
+}
